dd695eb29f06495945858548df0e05021d01df50
aogrodski <aogrodski@sandiego.edu>
2025-10-09 10:49:35 -0700

    got collisions working, added a notebook to plot

--- run_coll_MPI.sh
+++ run_coll_MPI.sh
@@ -2,6 +2,6 @@
 
 rm coll
 
-mpic++ test_collisions_MPI.cc collisions_MPI.cc collisions.cc mixed_dummy_vars.cc dummy_dep_vars.cc freqs.cc sterile.cc universe.cc -std=c++11 -o coll
+mpic++ test_collisions_MPI.cc collisions_MPI.cc collisions.cc mixed_dummy_vars.cc dummy_dep_vars.cc freqs.cc sterile.cc universe.cc sterile_decay.cc -std=c++11 -o coll
 
 mpiexec -n 6 coll
